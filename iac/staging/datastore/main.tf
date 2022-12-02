a99fc4dd8f5b2a6c371aa5fa3a7cf0e42a759dd6
Balaji Srinivasan <balaji.srini86@gmail.com>
2022-12-01 23:04:27 -0500

    Modularize datastore and webserver

--- iac/staging/datastore/main.tf
+++ iac/staging/datastore/main.tf
@@ -5,9 +5,28 @@ provider "aws" {
 module "datastore" {
   source = "../../modules/datastore"
 
-  bucket = "iac-terraform-state-madeforawscloud"
-  key = "staging-datastore-state"
-  env = "staging"
+  env         = "staging"
   db_username = "admin"
   db_password = "password123"
+}
+
+output "address" {
+  value       = module.datastore.address
+  description = "Endpoint of mysql db instance"
+}
+
+output "port" {
+  value       = module.datastore.port
+  description = "The port of the mysql db instance"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "iac-terraform-state-madeforawscloud"
+    key    = "staging/datastore/terraform.state"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
 }
